d7abbee08b3122d6b781a045625bdbd12abd4596
leiShengGuo <1362106037@qq.com>
2022-04-04 00:07:26 +0800

    init,4/4 add UserController

---
+++ second-hand-marked/src/main/java/com/mar/config/TotalExceptionHandle.java
@@ -0,0 +1,13 @@
+package com.mar.config;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * @author guokaifeng
+ * @createDate: 2022/4/4
+ **/
+
+@ControllerAdvice
+public class TotalExceptionHandle {
+
+}
---
+++ second-hand-marked/src/main/java/com/mar/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.mar.controller;
+
+/**
+ * @author guokaifeng
+ * @createDate: 2022/4/4
+ **/
+
+public class UserController {
+}
